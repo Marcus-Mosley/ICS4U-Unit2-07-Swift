67a92341d8b7eed93b2c1d2c1b9c9a1e74089e37
Marcus-Mosley <68971666+Marcus-Mosley@users.noreply.github.com>
2021-06-02 13:06:36 -0400

    Added in ICS4U-Unit2-07-Swift.xcodeproj

---
+++ ICS4U-Unit2-07-Swift/Shared/ICS4U_Unit2_07_Swift_App.swift
@@ -0,0 +1,17 @@
+//
+//  ICS4U_Unit2_07_Swift_App.swift
+//  Shared
+//
+//  Created by Marcus Mosley on 2021-06-02.
+//
+
+import SwiftUI
+
+@main
+struct ICS4U_Unit2_07_Swift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
